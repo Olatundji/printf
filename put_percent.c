ad89e2afcdbb88a379ab000ade9ac2ae7308eae1
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-07-13 14:18:01 +0100

    Update put_percent.c

--- put_percent.c
+++ put_percent.c
@@ -1,25 +1,13 @@
-nclude "main.h"
-
-
-
+#include "main.h"
 
 
 /**
- *
- * * put_percent - prints a percent character
- *
- * * @v: list of arguments
- *
- * * Return: total num printed
- *
- * */
-
+* put_percent - prints a percent character
+* @v: list of arguments
+* Return: total num printed
+*/
 int put_percent(__attribute__((unused))va_list v)
-
 {
-
-		_putchar('%');
-
-			return (1);
-
+	_putchar('%');
+	return (1);
 }
