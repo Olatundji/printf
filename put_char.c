71181af832863a38c7d155b1dbb9bd714565dec5
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-07-14 10:28:32 +0100

    Update put_char.c

--- put_char.c
+++ put_char.c
@@ -1,5 +1,12 @@
+#include "main.h"
+
+/**
+* put_char - specificer s
+* @v: list of arguments
+* Return: total characters
+*/
 int put_char(va_list v)
 {
-  _putchar(va_arg(v, int));
-  return(1);
+	_putchar(va_arg(v, int));
+	return (1);
 }
